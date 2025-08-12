dc2447ab4dd61b61368bafe4cfb6bcd87166b443
Dikshabharti06 <dikshabharti.db.06@gmail.com>
2025-08-12 11:23:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  // console.log(import.meta.env.VITE_KEY_VALUE);
+  console.log(import.meta.env);
+  return (
+    <>
+    <div>
+    Learning with appwrite</div>
+    </>
+  )
+}
+
+export default App
