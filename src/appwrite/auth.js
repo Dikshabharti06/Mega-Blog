48a45a8d20050af6e4fe6af4b3c16b638e53af40
Dikshabharti06 <dikshabharti.db.06@gmail.com>
2025-08-25 23:01:16 +0530

    auth commits

---
+++ src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import config from '../config/config.js';
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    Client = new Client();
+    //not putting id and endpoint here and making a constructor to utilise the prop of obj
+    Account;
+    constructor(){
+        this.Client
+        .setEndpoint(config.appwriteUrl)
+        .setProject(config.appwriteProjectId);
+        this.Account= new Account(this.Client);
+
+    }
+    async createAccount({email, password, name}){
+        try{
+            const userAccount= await this.Account.create(ID.unique(), email, password, name)
+            if(userAccount){
+                //call another method
+                return this.login({email, password});
+            }
+            else{
+                return userAccount;
+            }
+        }
+        catch(error){
+            throw error;
+        }
+    }
+    async login({email, password}){
+        try{        
+        //direct return without holding it in var
+        return await this.Account.createEmailPasswordSession(email, password);
+        }
+        catch(error){
+            throw error;
+        }
+    }
+    async getCurrentUser(){
+        try{
+          return await this.Account.get(); 
+        }
+        catch(error){
+            console.log("Appwrite Serive:: getCurrentUser::error", error);
+        }
+        //either use if else or return null
+        return null;
+    }
+    async logout(){
+        try{
+           return await this.Account.deleteSessions(); 
+        }
+        catch(error){
+            console.log("Appwrite Serive:: logout::error", error);
+        }
+    }
+}
+
+const authservice= new AuthService();
+
+export default authservice
